3b28b50fc4541349020af1cdb249e3aa16e30d7b
Jeeson101 <r0989261@student.vives.be>
2024-03-10 16:28:24 +0100

    Compare pagina aangemaakt
    Een compare button toegevoegd bij de charachters plus een compare pagina met 2 searches erbij gedaan

---
+++ Comic-Api/Comic-Api/Controllers/CompareController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comic_Api.Controllers
+{
+	public class CompareController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Index(string query)
+		{
+
+			return View();
+		}
+	}
+}
--- Comic-Api/Comic-Api/Controllers/HeroController.cs
+++ Comic-Api/Comic-Api/Controllers/HeroController.cs
@@ -10,6 +10,7 @@ namespace Comic_Api.Controllers
 	public class HeroController : Controller
 	{
 		private List<Hero> heroes;
+		bool NiksGegeven = false;
 
 		public HeroController()
 		{
@@ -27,7 +28,13 @@ namespace Comic_Api.Controllers
 		[HttpGet]
 		public ActionResult Search(string query)
 		{
-			List<Hero> results = new List<Hero>(); // Initialize a list to hold the search results
+			List<Hero> results = new List<Hero>();
+
+			if (string.IsNullOrEmpty(query))
+			{
+				ViewBag.ErrorMessage = "Search a hero please.";
+				return View(results);
+			}
 
 			if (!string.IsNullOrEmpty(query))
 			{
@@ -41,18 +48,47 @@ namespace Comic_Api.Controllers
 		[ValidateAntiForgeryToken]
 		public ActionResult SearchPost(string query)
 		{
-			var results = heroes.Where(h => h.name == (query.ToLower())).ToList();
+			List<Hero> results = new List<Hero>();
 
-			if (results.Any())
+			if (string.IsNullOrEmpty(query))
 			{
-				return View("Search", results);
+				NiksGegeven = true;
+				return RedirectToAction("Search");
 			}
-			else if (!string.IsNullOrEmpty(query))
+
+			query = query.ToLower();
+			query = query.TrimStart();
+			query = query.TrimEnd();
+
+			var exactMatches = heroes.Where(h => h.name.ToLower() == query).ToList();
+			results.AddRange(exactMatches);
+
+			if (exactMatches.Count == 0)
+			{
+				var KoppeltekenQuery = query.Replace(" ", "-");
+				var nietKoppeltekenQuery = query.Replace("-", " ");
+
+				var KoppeltekenMatch = heroes.Where(h => h.name.ToLower() == KoppeltekenQuery).ToList();
+				results.AddRange(KoppeltekenMatch);
+
+				if (KoppeltekenMatch.Count == 0)
+				{
+					var nietKoppeltekenMatch = heroes.Where(h => h.name.ToLower() == nietKoppeltekenQuery).ToList();
+					results.AddRange(nietKoppeltekenMatch);
+
+					if (nietKoppeltekenMatch.Count == 0)
+					{
+						results.AddRange(heroes.Where(h => h.name.ToLower().Contains(query)).ToList());
+					}
+				}
+			}
+
+			if (results.Count == 0)
 			{
 				ViewBag.ErrorMessage = "No results found.";
 			}
 
-			return View("Search");
+			return View("Search", results);
 		}
 	}
 }
