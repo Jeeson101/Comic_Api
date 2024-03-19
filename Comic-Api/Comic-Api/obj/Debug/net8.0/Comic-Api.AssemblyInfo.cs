2bc7c00f18dcfd8d2385bfb6c48c3f69530e66b8
TheRednax <milanderuytter@gmail.com>
2024-03-19 14:42:20 +0100

    Cleaning up and waiting for jason now

--- Comic-Api/Comic-Api/obj/Debug/net8.0/Comic-Api.AssemblyInfo.cs
+++ Comic-Api/Comic-Api/obj/Debug/net8.0/Comic-Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Comic-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101f26dd7d83a511b2a6ac4dd8dc08434f7ca8c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e6a4f7ba81d287648a28531d1f48a2611d2be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Comic-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Comic-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
