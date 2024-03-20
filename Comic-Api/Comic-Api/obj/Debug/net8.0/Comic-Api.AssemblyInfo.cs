79e0b0fbd869d1b8d150c0fb590b84b12b10401c
TheRednax <milanderuytter@gmail.com>
2024-03-20 12:55:28 +0100

    small things like text

--- Comic-Api/Comic-Api/obj/Debug/net8.0/Comic-Api.AssemblyInfo.cs
+++ Comic-Api/Comic-Api/obj/Debug/net8.0/Comic-Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Comic-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e6a4f7ba81d287648a28531d1f48a2611d2be8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc7c00f18dcfd8d2385bfb6c48c3f69530e66b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Comic-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Comic-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
